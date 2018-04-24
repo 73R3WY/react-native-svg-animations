75f1675ec58aca360ef05ca5eca34cab6cf71804
Samu András <samu.andris1@gmail.com>
2018-04-24 18:56:39 +0200

    fix(index.js): added OffsetSVGPath to module export

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import AnimatedSVGPath from './components/AnimatedSVGPath';
 import AnimatedSVGPaths from './components/AnimatedSVGPaths';
+import OffsetSVGPath from './components/OffsetSVGPath';
 
 /* Export ==================================================================== */
 
 module.exports = {
   AnimatedSVGPath: AnimatedSVGPath,
   AnimatedSVGPaths: AnimatedSVGPaths,
+  OffsetSVGPath: OffsetSVGPath,
 }
